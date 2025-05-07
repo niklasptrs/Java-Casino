9206809f3d54c3294a7aa4f2eea8ce5cce823979
niklasptrs <niklas.p.2007@gmail.com>
2025-05-07 16:56:05 +0200

    Dice v2 and roulette v1

--- dice.java
+++ dice.java
@@ -3,74 +3,73 @@ import java.util.Scanner;
 import java.util.Random;
 public class dice {
 
-
-Scanner scanner = new Scanner(System.in);
-int guthaben = 100;
-int betnum;
-int betwert;
-int dice;
-
+    Scanner scanner = new Scanner(System.in);
+    int guthaben = 100;
+    int betnum;
+    int betwert;
+    int dice;
 
 
-public void startGame(){
+    public void startGame(){
+        System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+        System.out.println("Auf welche Nummer wollen Sie setzen, wählen Sie zwischen 1-6.");
+        Scanner abfr = new Scanner(System.in);
+        betnum = abfr.nextInt();
 
-System.out.println("Auf welche Nummer wollen Sie setzen, wählen Sie zwischen 1-6.");
-Scanner abfr = new Scanner(System.in);
-betnum = abfr.nextInt();
+        if (betnum <=6 && betnum >=1){
 
-if (betnum <=6 && betnum >=1){
+            System.out.println("Wieviel wollen Sie auf "+betnum+" setzen?");
+            Scanner wert = new Scanner(System.in);
+            betwert = wert.nextInt();
 
-System.out.println("Wieviel wollen Sie auf "+betnum+" setzen?");
-Scanner wert = new Scanner(System.in);
-betwert = wert.nextInt();
+        }
 
-}
+        else if (betnum <1 || betnum >6){
+            System.out.println("Sie haben einen falschen Wert eingegeben, versuchen Sie erneut,");
+            System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+            startGame();
+        }
 
-else if (betnum <1 || betnum >6){
-System.out.println("Sie haben einen falschen Wert eingegeben, versuchen Sie erneut,");
-startGame();
-}
+        if(betwert <=guthaben){
 
-if(betwert <=guthaben){
+            int dice = ThreadLocalRandom.current().nextInt(1, 6 + 1);
 
-int dice = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+            if(dice == betnum){
+                System.out.println("Glückwunsch, es war "+ dice +" .");
+                int gewinn = betwert * 2;
 
-if(dice == betnum){
-System.out.println("Glückwunsch, es war "+ dice +" .");
-int gewinn = betwert * 2;
+                System.out.println("Du erhältst: "+gewinn+" €");
+                System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+                guthaben += gewinn;
+            }
 
-System.out.println("Du erhältst: "+gewinn+" €");
+            else if(dice != betnum){
+                System.out.println("Leider nicht richtig, es war "+dice+" .");
+                System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+                guthaben -= betwert;
+            }
 
-guthaben += gewinn;
-}
+        }
+        else if(betwert > guthaben || betwert < 1){
 
-else if(dice != betnum){
-System.out.println("Leider nicht richtig, es war "+dice+" .");
-guthaben -= betwert;
-}
+            System.out.println("Sie haben nicht genug Guthaben, oder einen ungültigen Wert eingegeben.");
+            startGame();
+            System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+        }
+        System.out.println("Wollen Sie noch einmal spielen?");
+        System.out.println("1: Ja");
+        System.out.println("2: Nein");
 
-}
-else if(betwert > guthaben || betwert < 1){
+        Scanner ergabfr = new Scanner(System.in);
+        int erg = ergabfr.nextInt();
 
-System.out.println("Sie haben nicht genug Guthaben, oder einen ungültigen Wert eingegeben.");
-startGame();
-}
-System.out.println("Wollen Sie noch einmal spielen?");
-System.out.println("1: Ja");
-System.out.println("2: Nein");
-
-Scanner ergabfr = new Scanner(System.in);
-int erg = ergabfr.nextInt();
-
-if(erg == 1){
-startGame();
-}
-else if( erg != 1){
-
-}
-}
-
-
+        if(erg == 1){
+            startGame();
+        }
+        else if( erg != 1){
+            System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
+        }
+    }
 
 
 }
---
+++ roulette.java
@@ -0,0 +1,60 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
+public class roulette
+{
+    
+
+    public roulette()
+    {
+        startGame();
+    }
+
+    public void startGame(){
+
+        schreiben("Auf was möchten Sie setzen?");
+        schreiben("1: Rot");
+        schreiben("2: Schwarz");
+        schreiben("3:Zahl");
+        
+        Scanner wettArt = new Scanner(System.in);
+        int wetteArt = wettArt.nextInt();
+        
+        if (wetteArt < 1 || wetteArt >3){
+            
+            schreiben("Sie haben einen ungültigen Wert eingegeben, versuchen Sie erneut.");
+            startGame();
+        }
+        else if(wetteArt == 2){
+            
+            int roulette = ThreadLocalRandom.current().nextInt(1, 36 + 1);
+            
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void schreiben(String pText){
+        String text = pText;
+
+        for (char buchstabe : text.toCharArray()) {
+            System.out.print(buchstabe); // Gibt Buchstaben einzeln aus
+            try {
+                Thread.sleep(30); // Pause von 100 Millisekunden
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+}
