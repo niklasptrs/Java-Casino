6be2e171c70648c4360311e1b57798e4eb792acc
niklasptrs <niklas.p.2007@gmail.com>
2025-05-07 18:09:30 +0200

    roulette v3

--- roulette.java
+++ roulette.java
@@ -2,7 +2,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
 public class roulette
 {
-    
+
     int guthaben = 100;
 
     public roulette()
@@ -13,91 +13,188 @@ public class roulette
     public void startGame(){
 
         schreiben("Auf was möchten Sie setzen?");
+        System.out.println("");
         schreiben("1: Rot");
+        System.out.println("");
         schreiben("2: Schwarz");
-        schreiben("3:Zahl");
-        
+        System.out.println("");
+        schreiben("3: Zahl");
+        System.out.println("");
+
         Scanner wettArt = new Scanner(System.in);
         int wetteArt = wettArt.nextInt();
-        
-schreiben("Wie viel möchten Sie setzen?");
-Scanner eingabeSatz = new Scanner(System.in);
-int einsatz = eingabeSatz.nextInt();
 
-if(einsatz > guthaben){
+        schreiben("Wie viel möchten Sie setzen?");
+        Scanner eingabeSatz = new Scanner(System.in);
+        int einsatz = eingabeSatz.nextInt();
 
-    schreiben("Sie haben einen ungültigen Wert eingegeben, versuchen Sie erneut.");
-            startGame();
-}
+        if(einsatz > guthaben){
 
-else if(einsatz <= guthaben){
-        if (wetteArt < 1 || wetteArt >3){
-            
             schreiben("Sie haben einen ungültigen Wert eingegeben, versuchen Sie erneut.");
+            System.out.println("");
             startGame();
         }
-        
-        else if(wetteArt == 1){
 
-            int roulette = ThreadLocalRandom.current().nextInt(1, 36 + 1);
-            
-            if(Arrays.stream(rot).anyMatch(s->s.eaquals(roulette))){
-int gewinn = einsatz * 2;
-schreiben("Herzlichen Glückwunsch Sie haben "+gewinn+" € gewonnen!")
-schreiben("Möchten Sie noch einmal spielen?");
-schreiben("1: Ja");
-schreiben("2: Nein");
+        else if(einsatz <= guthaben){
+            if (wetteArt < 1 || wetteArt >3){
 
-Scanner abfr = new Scanner(System.in);
-        int abfrage = abfr.nextInt();
-if(abfrage >= 2){
+                schreiben("Sie haben einen ungültigen Wert eingegeben, versuchen Sie erneut.");
+                System.out.println("");
+                startGame();
+            }
 
-}
-else if(abfrage == 1){
-    startGame();
-}
+            else if(wetteArt == 1){
+
+                int roulette = ThreadLocalRandom.current().nextInt(1, 36 + 1);
+
+                if(istInArray(rot, roulette)){
+                    int gewinn = einsatz * 2;
+                    schreiben("Herzlichen Glückwunsch Sie haben "+gewinn+" € gewonnen!");
+                    System.out.println("");
+                    schreiben("Möchten Sie noch einmal spielen?");
+                    System.out.println("");
+                    schreiben("1: Ja");
+                    System.out.println("");
+                    schreiben("2: Nein");
+                    System.out.println("");
+
+                    Scanner abfr = new Scanner(System.in);
+                    int abfrage = abfr.nextInt();
+                    if(abfrage >= 2){
+
+                    }
+                    else if(abfrage == 1){
+                        startGame();
+                    }
+
+                }
+
+                else if(!istInArray(rot, roulette)){
+                    schreiben("Du hast leider nicht gewonnen, es war "+roulette);
+                    System.out.println("");
+                    schreiben("Möchtest du eine neue Runde starten?");
+                    System.out.println("");
+                    schreiben("1: Ja");
+                    System.out.println("");
+                    schreiben("2: Nein");
+
+                    Scanner abfr = new Scanner(System.in);
+                    int abfrage = abfr.nextInt();
+                    if(abfrage >= 2){
+
+                    }
+                    else if(abfrage == 1){
+                        startGame();
+                    }
+                }
 
             }
-           
+
+            else if(wetteArt == 2){
+                int roulette = ThreadLocalRandom.current().nextInt(1, 36 + 1);
+                if(istInArray(schwarz, roulette)){
+                    int gewinn = einsatz * 2;
+                    schreiben("Herzlichen Glückwunsch Sie haben "+gewinn+" € gewonnen!");
+                    System.out.println("");
+                    schreiben("Möchten Sie noch einmal spielen?");
+                    System.out.println("");
+                    schreiben("1: Ja");
+                    System.out.println("");
+                    schreiben("2: Nein");
+                    System.out.println("");
+
+                    Scanner abfr = new Scanner(System.in);
+                    int abfrage = abfr.nextInt();
+                    if(abfrage >= 2){
+
+                    }
+                    else if(abfrage == 1){
+                        startGame();
+                    }
+
+                }
+
+                else if(!istInArray(schwarz, roulette)){
+                    schreiben("Du hast leider nicht gewonnen, es war "+roulette);
+                    System.out.println("");
+                    schreiben("Möchtest du eine neue Runde starten?");
+                    System.out.println("");
+                    schreiben("1: Ja");
+                    System.out.println("");
+                    schreiben("2: Nein");
+
+                    Scanner abfr = new Scanner(System.in);
+                    int abfrage = abfr.nextInt();
+                    if(abfrage >= 2){
+
+                    }
+                    else if(abfrage == 1){
+                        startGame();
+                    }
+                }
+            } 
+            else if(wetteArt == 3){
+
+                schreiben("Auf welche Zahl möchtest du setzen, wähle zwischen 0 und 36.");
+                System.out.println("");
+
+                Scanner abfr1 = new Scanner(System.in);
+                int wette = abfr1.nextInt();
+
+                if(wette > 36){
+                    schreiben("Ungültige Eingabe, versuche erneut.");
+                    startGame();
+                }
+
+                else if(wette >= 0 && wette <= 36){
+                    int roulette = ThreadLocalRandom.current().nextInt(1, 36 + 1);
+
+                    if(true){
+
+                        int gewinn = einsatz + einsatz * 35;
+                        schreiben("Herzlichen Glückwunsch Sie haben "+gewinn+" € gewonnen!");
+                        System.out.println("");
+                        schreiben("Möchten Sie noch einmal spielen?");
+                        System.out.println("");
+                        schreiben("1: Ja");
+                        System.out.println("");
+                        schreiben("2: Nein");
+                        System.out.println("");
+
+                        Scanner abfr = new Scanner(System.in);
+                        int abfrage = abfr.nextInt();
+                        if(abfrage >= 2){
+
+                        }
+                        else if(abfrage == 1){
+                            startGame();
+                        }
+
+                    }
+                    else if(wette != roulette){
+                        schreiben("Du hast leider nicht gewonnen, es war "+roulette);
+                        System.out.println("");
+                        schreiben("Möchtest du eine neue Runde starten?");
+                        System.out.println("");
+                        schreiben("1: Ja");
+                        System.out.println("");
+                        schreiben("2: Nein");
+
+                        Scanner abfr = new Scanner(System.in);
+                        int abfrage = abfr.nextInt();
+                        if(abfrage >= 2){
+
+                        }
+                        else if(abfrage == 1){
+                            startGame();
+                        }
+
+                    }
+                }
+            }
         }
-        
-        
-        
-        else if(wetteArt == 2){
-            
-            int roulette = ThreadLocalRandom.current().nextInt(1, 36 + 1);
-            
-            if(Arrays.stream(schwarz).anyMatch(s->s.eaquals(roulette))){
-int gewinn = einsatz * 2;
-schreiben("Herzlichen Glückwunsch Sie haben "+gewinn+" € gewonnen!")
-schreiben("Möchten Sie noch einmal spielen?");
-schreiben("1: Ja");
-schreiben("2: Nein");
-
-Scanner abfr = new Scanner(System.in);
-        int abfrage = abfr.nextInt();
-if(abfrage >= 2){
-
-}
-else if(abfrage == 1){
-    startGame();
-}
-
-            }
-           }   }
     }
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     public void schreiben(String pText){
         String text = pText;
 
@@ -112,21 +209,26 @@ else if(abfrage == 1){
         }
     }
 
+    public static boolean istInArray(int[] array, int zahl) {
+        for (int z : array) {
+            if (z == zahl) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    int[] schwarz = {
+            2, 4, 6, 8, 10,
+            11, 13, 15, 17, 20,
+            22, 24, 26, 28, 29,
+            31, 33, 35
+        };
 
-
-
-int[] schwarz = {
-    2, 4, 6, 8, 10,
-    11, 13, 15, 17, 20,
-    22, 24, 26, 28, 29,
-    31, 33, 35
-};
-
-int[] rot = {
-    1, 3, 5, 7, 9,
-    12, 14, 16, 18, 19,
-    21, 23, 25, 27, 30,
-    32, 34, 36
-};
+    int[] rot = {
+            1, 3, 5, 7, 9,
+            12, 14, 16, 18, 19,
+            21, 23, 25, 27, 30,
+            32, 34, 36
+        };
 }
