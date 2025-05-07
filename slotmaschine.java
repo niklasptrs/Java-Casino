ca4a0746634cf0c8ce4387086232db55548efebd
niklasptrs <niklas.p.2007@gmail.com>
2025-05-07 21:12:38 +0200

    slotmachine v1

---
+++ slotmaschine.java
@@ -0,0 +1,47 @@
+public slotmaschine{
+
+
+String  [] Symbole = {
+
+    Symbole[0] = " ________ 
+|        \
+ \$$$$$$$$
+    /  $$ 
+   /  $$  
+  /  $$   
+ /  $$    
+|  $$     
+ \$$      ";
+
+
+
+}
+
+public class slotmaschine(){
+
+    startGame();
+}
+
+public void startGame(){
+
+
+
+
+
+}
+
+
+public void test(){
+
+    System.out.println(Symbole[0]);
+}
+
+
+
+
+
+
+
+
+
+}
