0e66bb361842c663a2eba79ae44a9b90ec0f82ee
niklasptrs <niklas.p.2007@gmail.com>
2025-05-09 13:26:25 +0200

    bug fixes

--- slotmaschine.java
+++ slotmaschine.java
@@ -1,23 +1,13 @@
-public slotmaschine{
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+public class slotmaschine{
 
+int speed = 30;
+int guthaben = 100;
 
-String  [] Symbole = {
+String[] Symbole = {"0", "&", "€", "§", "$", "#", "7"};
 
-    Symbole[0] = " ________ 
-|        \
- \$$$$$$$$
-    /  $$ 
-   /  $$  
-  /  $$   
- /  $$    
-|  $$     
- \$$      ";
-
-
-
-}
-
-public class slotmaschine(){
+public  slotmaschine(){
 
     startGame();
 }
@@ -25,21 +15,153 @@ public class slotmaschine(){
 public void startGame(){
 
 
+System.out.println("Wieviel möchten Sie setzen?");
+Scanner eins = new Scanner(System.in);
+int einsatz = eins.nextInt();
 
+
+if(einsatz > guthaben){
+System.out.println("Ungültige Eingabe, versuchen Sie erneut.");
+startGame();
+
+
+}
+else if(einsatz <= guthaben){
+
+int slot1 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+int slot2 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+int slot3 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+int slot4 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+int slot5 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+
+speed = 100;
+schreiben(Symbole[slot1]);
+speed = 1;
+schreiben("=====");
+speed = 100;
+schreiben(Symbole[slot2]);
+speed = 1;
+schreiben("=====");
+speed = 100;
+schreiben(Symbole[slot3]);
+speed = 1;
+schreiben("=====");
+speed = 100;
+schreiben(Symbole[slot4]);
+speed = 1;
+schreiben("=====");
+speed = 100;
+schreiben(Symbole[slot5]);
+speed = 30;
+
+
+if(slot1 != slot2 && slot2 != slot3 && slot3 != slot4 && slot4 != slot5){
+
+schreiben("-"+einsatz+" €");
+guthaben -= einsatz;
+weiter();
+
+if (slot1 == slot2 && slot2 == slot3 && slot3 == slot4 && slot4 == slot5) {
+
+
+    schreiben("Jackpot! +" + einsatz * 15 + " €");
+
+
+    guthaben += einsatz * 15;
+
+weiter();
+}
+//Wenn 345 richtig ist
+if(slot3 == slot4 && slot4 == slot5){
+
+schreiben("WIN!!!");
+System.out.println("");
+schreiben("+ "+einsatz * 10+" €");
+
+guthaben += einsatz * 10;
+
+weiter();
+
+}
+
+
+//Wenn die 234 richtig ist
+if(slot2 == slot3 && slot3 == slot4){
+
+schreiben("WIN!!!");
+System.out.println("");
+schreiben("+ "+einsatz * 10+" €");
+
+guthaben += einsatz * 10;
+
+weiter();
+}
+
+
+// Wenn die ersten drei richtig sind
+if(slot1 == slot2 && slot2 == slot3){
+
+schreiben("WIN!!!");
+System.out.println("");
+schreiben("+ "+einsatz * 10+" €");
+
+guthaben += einsatz * 10;
+
+weiter();
+}
+
+}
 
 
 }
 
 
-public void test(){
-
-    System.out.println(Symbole[0]);
 }
 
 
 
 
+public void schreiben(String pText){
 
+        String text = pText;
+
+
+
+        for (char buchstabe : text.toCharArray()) {
+
+            System.out.print(buchstabe); // Gibt Buchstaben einzeln aus
+
+            try {
+
+                Thread.sleep(speed); // Pause von 100 Millisekunden
+
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+
+            }
+
+
+
+        }
+
+    }
+
+public void weiter(){
+
+schreiben("Noch einmal spielen?");
+System.out.println("");
+System.out.println("1: Ja");
+System.out.println("2: Nein");
+
+Scanner abfr = new Scanner(System.in);
+int abfrage = abfr.nextInt();
+
+if(abfrage >=2){}
+if(abfrage == 1){
+startGame();
+}
+}
 
 
 
