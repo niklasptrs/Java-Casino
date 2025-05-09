b79fee567863582268b1714d548eb9cc642b608d
niklasptrs <niklas.p.2007@gmail.com>
2025-05-09 10:09:15 +0200

    Casino.java aktualisieren

--- Casino.java
+++ Casino.java
@@ -7,7 +7,9 @@ slotmaschine slotmaschine1;
 
 
 public Casino{
+boolean valid = false;
 
+while(!valid){
 System.out.println("");
 System.out.println("Was möchten Sie spielen?");
 System.out.println("1: Dice");
@@ -17,19 +19,23 @@ System.out.println("4: Fragen);
 
 Scanner abfr = new Scanner(System.in);
 int abfrage = abfr.nextInt();
-
+}
 if(abfrage > 4 || abfrage < 1){
+
 System.out.println("Ungültige Eingabe, versuchen Sie erneut.");
 
 }
 else if(abfrage == 1){
 dice1.startGame();
+valid = true;
 }
 else if(abfrage == 2){
 roulette1.startGame();
+valid = true;
 }
 else if(abfrage == 3){
 slotmaschine1.startGame();
+valid = true;
 }
 
 
