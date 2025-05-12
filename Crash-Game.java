eb785c0a31ff7f58c5815e7552eb50fb3f506514
niklasptrs <niklas.p.2007@gmail.com>
2025-05-12 09:03:04 +0200

    Create Crash-Game.java

---
+++ Crash-Game.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Crash-Game{
+int Guthaben = 100;
+public Crash-Game(){
+
+
+}
+
+public void startGame(){
+
+System.out.println("Wieviel möchten Sie setzen?");
+  Scanner abfr = new Scanner(System.in);
+  int abfrage = abfr.nextInt();
+
+}
+
+}
