e0d3660932aabf6695e84b71b5c0f0adc6056fa7
niklasptrs <niklas.p.2007@gmail.com>
2025-07-02 08:35:38 +0200

    Update BlackJack.java
    preparation for new Card Library

--- BlackJack.java
+++ BlackJack.java
@@ -1,35 +1,15 @@
-import io.lyuda.jcards.*;
+
 import java.util.Scanner;
 public class BlackJack
 {
-    Card karten [];
-    Deck deck1;
-    int summe;
-    int aktuelleKarten;
-    Card hands [];
+   
     public BlackJack()
     {
-        aktuelleKarten = 0;
-        deck1 = new Deck();
-        deck1.shuffle();
-        karten = new Card [52];
-
-        for(int i = 0; i < 52 ;i++){
-            karten[i] = deck1.deal();
-        }
+       
     }
 
     public void startGame(){
-        
-        System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
-        System.out.println("Deine Karten sind");
-        System.out.println(karten[0]);
-        System.out.println(karten[1]);
-        System.out.println("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»");
-        System.out.println("Die Karten des Dealers sind");
-        System.out.println(karten[2]);
-        //karten[3]
-        aktuelleKarten = 4;
+      
         
         
 
@@ -43,8 +23,7 @@ public class BlackJack
         char abfrage = abfr.toUpperCase().charAt(0);
         
         if(abfrage == 'H'){
-            System.out.println(karten[aktuelleKarten]);
-            aktuelleKarten += 1;
+           
         }
         if(abfrage == 'S'){
             
